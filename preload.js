5550c6b8e25bf7faeeb82b2a9de5b5b76af6aec3
Kaíke Petalas Nóbrega <kaike@VRP65071275.acad.unifor.br>
2025-03-24 15:43:47 -0300

    Foram feitos ajustes no html

--- preload.js
+++ preload.js
@@ -2,11 +2,9 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 // Expor API segura para o renderer
 contextBridge.exposeInMainWorld('electronAPI', {
+  // Funções adicionais que você quiser expor para o renderer
+  openAndExecuteFileByPath: (filePath) => ipcRenderer.send('open-file-by-path', filePath),
+
   // Função para baixar jogos que não necessita mais do seletor
   downloadGame: (url) => ipcRenderer.invoke('download-game', url),
-  // Funções adicionais que você quiser expor para o renderer
-  createFolder: () => ipcRenderer.send('create-folder'),
-  openAndExecuteFileByPath: (filePath) => ipcRenderer.send('open-file-by-path', filePath),
-  onExecutionResult: (callback) => ipcRenderer.on('execution-result', callback),
-  onFolderCreated: (callback) => ipcRenderer.on('folder-created', callback),
 });
